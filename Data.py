99d6f614b2695a5a16864c888d701390f2e24370
Tigebros <markanthiny47@gmail.com>
2024-11-01 11:49:58 +0100

    upload other files

---
+++ Data.py
@@ -0,0 +1,10 @@
+#install all the necessary packages
+pip install matplotlib 
+pip install numpy
+pip install pandas
+pip install seaborn
+#import all the necessary packages into the code
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
